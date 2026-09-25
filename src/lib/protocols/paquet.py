4a356d3fbc1c91c6d54d2b32fd99a869a239bbe5
tomimodesti~ <tomimodesti@gmail.com>
2026-09-24 23:23:28 -0300

    acomodando el paquete

--- src/lib/protocols/paquet.py
+++ src/lib/protocols/paquet.py
@@ -5,16 +5,18 @@ RDT Packet Header
             0                   1                   2                   3
             0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
             +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-            |VERSION|PROTOCOL|    CHECKSUM                 |  FLAGS         |
+            |VERSION|PROTOCOL|  FLAGS     | hlen(bytes)    |  RESERVED      |
             +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-            |          SEQUENCE NUMBER        |          ACK                |
+            |                   Sequence number                             |
             +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-            |     CWND      |                      Reserved                 |
+            |                           ACK                                 |
+            +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+            |                            OPTIONS                            |
             +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
             |                              PAYLOAD                          |
             +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
-Header size: 12 bytes (96 bits)
+Minimun Header size: 12 bytes (96 bits)
 
 Fields:
     VERSION:
@@ -23,14 +25,10 @@ Fields:
 
     PROTOCOL:
         RDT reliability protocol in use.
-        4 bits. (prevee un posible protocolo nuevo, es escalable a futuro).
+        4 bits.
         Examples:
             STOP_AND_WAIT
             SELECTIVE_ACK
-
-    CHECKSUM:
-        Checksum used to detect corrupted RDT packets.
-        16 bits.
 
     FLAGS:
         Control flags.
@@ -39,34 +37,40 @@ Fields:
             7 6 5 4 3 2 1 0
             7: SYN - initialization / negotiation
             6: FIN - end of transfer
-            ...
+            5: ERR - error in packet
+            4 - 0: reserved for future use
 
+    HLEN:
+        Header length (in bytes).
+        8 bits.
+           
     SEQUENCE NUMBER:
-        Sequence number identifica el numero de paquete, numero rotativo.
-        16 bits.
+        Sequence number indicates the order of the packet in the stream.
+        32 bits.
 
     ACK:
         Sequence number being acknowledged.
-        16 bits.
-
-    CWND:
-        Current congestion/control window information.
-        Representa el numero de paquetes que puede recibir (temas de memoria).
-        8 bits.
+        32 bits.
+    
+    OPTIONS:
+        SACK - [x,y]
 
     PAYLOAD:
-        El resto de espacio del paquete son los datos que envia la aplicacion.
+        The data being transmitted (application data).
 
-Tamaño del paquete: depende el MTT (Maximum Transmission Unit)
-ejemplo usando MTT = x Bytes
+Size of packet MTT (Maximum Transmission Unit)
+example using MTT = x Bytes
 x - 20 Bytes (IP header) - 8 Bytes (UDP header) - 12 Bytes (RDT header) = x - 40 Bytes
-x - 40 Bytes = tamaño maximo de payload que puede enviar la app usadando este protocolo.
+x - 40 Bytes = maximum size of payload that can be sent by the application using this protocol.
 """
 
 # Estas funciones se basan en utilizar funciones de bytes
 # Como AND, SHIFT, etc. Aprovechando que estamos trabajando con bytes.
 
-HEADER_SIZE = 12
+SYN_MASK = 0b10000000
+FIN_MASK = 0b01000000
+ERR_MASK = 0b00100000
+
 
 def get_header_version(packet: bytes) -> int:
     """Get the version of the RDT protocol from the packet header."""
@@ -81,20 +85,11 @@ def get_header_protocol(packet: bytes) -> int:
     return packet[0] & 0x0F
 
 
-def get_header_checksum(packet: bytes) -> int:
-    """Get the checksum of the RDT protocol from the packet header."""
-    return int.from_bytes(packet[1:3], "big")
-
-
 def get_header_flags(packet: bytes) -> int:
     """Get the flags of the RDT protocol from the packet header."""
     # Devuelve todos los bytes correspondientes a los flags, luego con
     # operaciones de and se puede obtener cada flag
-    return packet[3]
-
-
-SYN_MASK = 0b10000000
-FIN_MASK = 0b01000000
+    return packet[1]
 
 
 def get_flag_SYN(flags: bytes) -> int:
@@ -106,27 +101,40 @@ def get_flag_SYN(flags: bytes) -> int:
 def get_flag_FIN(flags: bytes) -> int:
     """"Get the flag SYN from the flags of the packet"""
     # 0x00 0000 ---> x y le hago el and con 1
-    # haciendo con mascara return (flags[0] & 0b10000000)
+    # haciendo con mascara return (flags[0] & FIN_MASK)
     return (flags[0] >> 6) & 1
+
+
+def get_flag_ERR(flags: bytes) -> int:
+    """"Get the flag ERR from the flags of the packet"""
+    # 0x00 0000 ---> x y le hago el and con 1
+    # haciendo con mascara return (flags[0] & ERR_MASK)
+    return (flags[0] >> 5) & 1
+
+
+def get_header_hlen(packet: bytes) -> int:
+    return packet[2]
+
+
+def get_header_options(packet: bytes) -> bytes:
+
+    hlen = get_header_hlen(packet)
+
+    if hlen > 12:
+        return packet[12:hlen]
+    return b"" #No options
 
 
 def get_header_sequence_paquet(packet: bytes) -> int:
     """Get the sequence paquet of the RDT protocol from the packet header."""
-    return int.from_bytes(packet[4:6], "big")
+    return int.from_bytes(packet[4:8], "big")
 
 
 def get_header_ack(packet: bytes) -> int:
     """Get the ack of the RDT protocol from the packet header."""
-    return int.from_bytes(packet[6:8], "big")
+    return int.from_bytes(packet[8:12], "big")
 
 
-def get_header_cwind(packet: bytes) -> int:
-    """Get the cwind of the RDT protocol from the packet header."""
-    return packet[8]
-
-
-def get_paquet_payload(packet: bytes) -> bytes:
-    """"Get the payload of the RDT protocol from the packet"""
-    if len(packet) < HEADER_SIZE:
-        raise ValueError("Paquete menor al tamaño del header")
-    return packet[HEADER_SIZE:]
+def get_payload(packet: bytes) -> bytes:
+    hlen = get_header_hlen(packet)
+    return packet[hlen:]
